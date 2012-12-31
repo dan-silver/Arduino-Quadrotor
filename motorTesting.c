d06c3ef178185bb16636a8ec9409597cb972d10b
Dan Silver <dannysilver3@gmail.com>
2012-12-30 23:53:44 -0600

    Multiple motors, hold all motors at set speed

--- motorTesting.c
+++ motorTesting.c
@@ -1,35 +1,51 @@
 //Calibrates the electronic speed controler (esc) and send different speeds to the motors
 #include <Servo.h>
 
-Servo motor;
+Servo motor[2];
 
-#define PIN_MOTOR	9
+#define PIN_MOTOR_1	9
+#define PIN_MOTOR_2	10
 
 void armSpeedControler() {
-  delay(5000);
-  Serial.println("Sending lowest throttle");
-  motor.writeMicroseconds(1000);
-  delay(2000);
-  Serial.println("Low throttle sent");
+	delay(5000);
+	Serial.println("Sending lowest throttle to all motors");
+	motor[0].writeMicroseconds(1000);
+	motor[1].writeMicroseconds(1000);
+	delay(2000);
+	Serial.println("Low throttle sent");
+}
+
+//holds all motors at a constant speed
+void constantSpeed(int speed) {
+	motor[0].writeMicroseconds(speed);
+	motor[1].writeMicroseconds(speed);
+	Serial.print("Running motors at ");
+	Serial.println(speed);
 }
 
 void setup() {
-  Serial.begin(9600);
-  motor.attach(PIN_MOTOR);
-  Serial.println("ESC calibration started"); 
-  armSpeedControler();
-  Serial.println("ESC calibration completed");
+	Serial.begin(9600);
+	motor[0].attach(PIN_MOTOR_1);
+	motor[1].attach(PIN_MOTOR_2);
+	Serial.println("ESC calibration started"); 
+	armSpeedControler();
+	Serial.println("ESC calibration completed");
 }
 
 void loop() {
-      for (int i=1000;i<=1450;i+=5) {
-        motor.writeMicroseconds(i);	
-        Serial.println(i);
-        delay(100);
-      }
-      for (int i=1450;i>=1000;i-=5) {
-        motor.writeMicroseconds(i);	
-        Serial.println(i);
-        delay(100);
-      }
+	constantSpeed(1050);
+/**	for (int i=1000;i<=1350;i+=5) {
+		motor[0].writeMicroseconds(i);
+		motor[1].writeMicroseconds(i);
+		Serial.println(i);
+		delay(100);
+	}
+	for (int i=1350;i>=1000;i-=5) {
+		motor[0].writeMicroseconds(i);
+		motor[1].writeMicroseconds(i);
+		Serial.println(i);
+		delay(100);
+	}
+**/
+	delay(100);
 }
