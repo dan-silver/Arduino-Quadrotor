63fb5e24b1162501bbf462a086d72e0dd8e56493
Dan Silver <dannysilver3@gmail.com>
2013-01-06 23:35:20 -0600

    Read in accelerometer data

--- motorTesting/motorTesting.ino
+++ motorTesting/motorTesting.ino
@@ -12,6 +12,24 @@ int motorVelocities[4] = {1000};
 #define PIN_MOTOR_3	9
 #define PIN_MOTOR_4	11
 
+
+
+//Analog read pins
+const int xPin = 0;
+const int yPin = 1;
+const int zPin = 2;
+
+//The minimum and maximum values that came from
+//the accelerometer while standing still
+//You very well may need to change these
+int minVal =270;
+int maxVal =440;
+
+//to hold the caculated values
+double x;
+double y;
+double z;
+
 void armSpeedControler() {
 	Serial.println("Sending lowest throttle to all motors");
         for (int i=0;i<4;i++) {
@@ -66,8 +84,36 @@ void setup() {
 	timer.setTimeout(9000, killAllMotors);
 }
 
+void readAccelerometer() {
+   //read the analog values from the accelerometer
+  int xRead = analogRead(xPin);
+  int yRead = analogRead(yPin);
+  int zRead = analogRead(zPin);
+
+  //convert read values to degrees -90 to 90 - Needed for atan2
+  int xAng = map(xRead, minVal, maxVal, -90, 90);
+  int yAng = map(yRead, minVal, maxVal, -90, 90);
+  int zAng = map(zRead, minVal, maxVal, -90, 90);
+
+  //Caculate 360deg values like so: atan2(-yAng, -zAng)
+  //atan2 outputs the value of -PI to PI (radians)
+  //We are then converting the radians to degrees
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+
+  //Output the caculations
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print(" | y: ");
+  Serial.print(y);
+  Serial.print(" | z: ");
+  Serial.println(z);
+}
+
 void loop() {
 	timer.run();
+        readAccelerometer();
 	for (int i=0;i<4;i++) {
 		motor[i].writeMicroseconds(motorVelocities[i]);
 	}
